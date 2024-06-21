8e77de88c31d239711fd08b7045d17a22f6e4f45
Scott Wilson <scottwilson0903@gmail.com>
2024-06-21 02:26:27 +0100

    Minor Changes

--- WizGunCosmeticsExample.cs
+++ WizGunCosmeticsExample.cs
@@ -24,16 +24,7 @@ namespace WizGunCosmeticsExample
 
             Log.LogInfo("Cosmetics Example Mod Loaded");
 
-            GlamourManager.New(ItemType.Robes, "custom", "Custom Robes", "Custom Robes by Cyantist")
-                                            .AddIcon(Path.Combine(ModPath, "Assets/robes-custom.png"))
-                                            .AddSkin(SkinID.BodyForward, Path.Combine(ModPath, "Assets/custom-body-forward.png"))
-                                            .AddSkin(SkinID.BodyBackward, Path.Combine(ModPath, "Assets/custom-body-backward.png"))
-                                            .AddSkin(SkinID.ArmLeftForward, Path.Combine(ModPath, "Assets/custom-arm-left-forward.png"))
-                                            .AddSkin(SkinID.ArmLeftBackward, Path.Combine(ModPath, "Assets/custom-arm-left-backward.png"))
-                                            .AddSkin(SkinID.ArmRightForward, Path.Combine(ModPath, "Assets/custom-arm-right-forward.png"))
-                                            .AddSkin(SkinID.ArmRightBackward, Path.Combine(ModPath, "Assets/custom-arm-right-backward.png"));
-
-            GlamourManager.New(ItemType.Robes, "test", "Test Robes", "Test Robes by Cyantist")
+            GlamourManager.New(ItemType.Robes, "custom", "Custom Robes", "Custom Example Robes")
                                             .AddIcon(Path.Combine(ModPath, "Assets/robes-custom.png"))
                                             .AddSkin(SkinID.BodyForward, Path.Combine(ModPath, "Assets/custom-body-forward.png"))
                                             .AddSkin(SkinID.BodyBackward, Path.Combine(ModPath, "Assets/custom-body-backward.png"))
